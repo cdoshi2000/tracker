1e6a17ec5a0ea7c229965136f886ab4349bda47f
Chirag Doshi <chirag@doshi.cc>
2020-04-10 02:26:32 +0300

    update the readme

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=cigo',
     'username' => 'root',
-    'password' => 'cdosh!1234',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
